c581ff38a8d722e8ea686d033df1ddf7dd9205b8
Jehiverson Rodriguez <Jrodriguezt7@miumg.edu.gt>
2020-08-24 20:57:29 -0600

    La maravilla 2.0

--- controllers/BinnacleSaleController.js
+++ controllers/BinnacleSaleController.js
@@ -15,7 +15,7 @@ async function getBinnacleSale(req, res) {
 async function getBinnacleSaleReport(req, res) { 
     let sales = await BinnacleSaleByte.find({
         store_creat:"Guess Oakland"
-    }).sort({ date_created: -1 }).limit(50);
+    }).sort({ date_created: -1 });
     
     return res.json({sales});
 }
